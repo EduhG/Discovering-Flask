7d52361a6c03b379f95c7bc1eb4e4c995c06a771
eduhg <edwinnyangena@gmail.com>
2016-08-19 23:09:21 +0300

    Added index.html file

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, flash, redirect, session
 
 app = Flask(__name__)
+
+app.secret_key = 'am mean'
 
 
 @app.route('/')
 def home():
-    return 'Hello World!'
+    return render_template('index.html')
 
 
 @app.route('/welcome')
@@ -16,12 +18,22 @@ def welcome():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
-    if request.method is 'POST':
+    if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid credentials please try again'
+            error = 'Invalid credentials. Please try again'
         else:
+            session['logged_in'] = True
+            flash('You were just logged in')
             return redirect(url_for('home'))
+
     return render_template('login.html', error=error)
+
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were just logged out')
+    return redirect(url_for('welcome'))
 
 if __name__ == '__main__':
     app.run(debug=True)
